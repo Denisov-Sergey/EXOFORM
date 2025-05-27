37d4519a9c763b02829980347cc8fb5722c14ddc
Jay <sithis206@gmail.com>
2025-05-27 23:41:51 +0600

    add OnSettingsChanged event

--- Assets/WorldGenerator/Abstract/NoiseSettings.cs
+++ Assets/WorldGenerator/Abstract/NoiseSettings.cs
@@ -5,6 +5,18 @@ namespace WorldGenerator.Abstract
 {
     public abstract class NoiseSettings : ScriptableObject
     {
+        public event System.Action OnSettingsChanged;
+
+        protected virtual void OnValidate()
+        {
+            OnSettingsChanged?.Invoke();
+        }
+    
+        private void OnEnable()
+        {
+            OnSettingsChanged = null; // Очищаем при загрузке
+        }
+        
         [Header("Noise Settings")] 
         [Tooltip("Сид для генерации шума. Измените, чтобы получить другую карту.")]
         public int seed = 12345;
