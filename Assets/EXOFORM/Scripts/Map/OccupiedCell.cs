320d9ca1673e56a795efccafebe8acae2918900d
Jay <sithis206@gmail.com>
2025-07-07 01:07:53 +0600

    Добавлены типы ячеек

---
+++ Assets/EXOFORM/Scripts/Map/OccupiedCell.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Exoform.Scripts.Map
+{
+    public struct OccupiedCell
+    {
+        public Vector2Int Cell;
+        public int Rotation;
+
+        public OccupiedCell(Vector2Int cell, int rotation)
+        {
+            Cell = cell;
+            Rotation = rotation;
+        }
+    }
+}
